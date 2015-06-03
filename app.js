e923452bbed5cb0dfcb4d529655839a328d75ef5
dizehacioglu <dh1272@nyu.edu>
2015-06-03 12:07:18 -0600

    added port

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://heroku_app37479949:30boehbjbsq593slfhp30358g9@ds031872.mongolab.com:31872/heroku_app37479949")
+
+
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -11,6 +15,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', indexController.index);
 app.post('/renderLocation', indexController.renderLocation);
 
-var server = app.listen(9849, function() {
+var port = process.env.PORT || 9849;
+
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
